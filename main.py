9751a970ea458e4b57b9b72642f4baa837d63a07
狐日泽 <shdocter@nya-wsl.com>
2025-06-19 00:29:58 +0800

    v1.2.2
    - 临时修复B站API增加了WBI签名而无法获取弹幕的bug，等待上游更新
    
    - 将Python升级至3.13.5

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from nicegui import ui, app
 
 from blivedm import blivedm
 
-version = "1.2.1"
+version = "1.2.2"
 b_connect_status = False # 初始化弹幕服务器连接状态
 app.add_static_files('/static', 'static')
 
